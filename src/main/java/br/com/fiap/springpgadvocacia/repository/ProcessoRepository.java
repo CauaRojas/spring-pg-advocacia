b9146049091b3544f20da1da43c442c7eec0954b
oleoz1n <leopity0@gmail.com>
2024-03-05 08:27:34 -0300

    fix: fixing repository annotation

--- src/main/java/br/com/fiap/springpgadvocacia/repository/ProcessoRepository.java
+++ src/main/java/br/com/fiap/springpgadvocacia/repository/ProcessoRepository.java
@@ -1,4 +1,7 @@
 package br.com.fiap.springpgadvocacia.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ProcessoRepository extends JpaRepository<Processo, Long>{
 }
